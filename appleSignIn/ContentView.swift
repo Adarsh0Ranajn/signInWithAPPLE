b410a502460e5e600b14cc0f340fb45c08dc8fb0
adarsh <adarsh0ranjan@gmail.com>
2022-08-08 09:05:57 +0530

    Initial Commit

---
+++ appleSignIn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appleSignIn
+//
+//  Created by Roro Solutions on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appleSignIn/appleSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleSignInApp.swift
+//  appleSignIn
+//
+//  Created by Roro Solutions on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct appleSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
