b95942d442cf5608e786849f96caa6c9d9a1f34f
adarsh <adarsh0ranjan@gmail.com>
2022-08-08 12:00:37 +0530

    first commit

--- appleSignIn/ContentView.swift
+++ appleSignIn/ContentView.swift
@@ -4,13 +4,59 @@
 //
 //  Created by Roro Solutions on 08/08/22.
 //
-
+import AuthenticationServices
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    @AppStorage("email") var email : String = ""
+    @AppStorage("firstName") var firstName : String = ""
+    @AppStorage("lastName") var lastName : String = ""
+    @AppStorage("userID") var userID : String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            VStack{
+                SignInWithAppleButton(.continue){ request in
+                    request.requestedScopes = [.email, . fullName]
+                    
+                }  onCompletion: { result in
+                    switch result {
+                    case .success(let auth):
+                        switch  auth.credential{
+                        case let credential as ASAuthorizationAppleIDCredential:
+                            let userId = credential.user
+                            
+                            let email = credential.email
+                            let firstName = credential.fullName?.givenName
+                            let lastName = credential.fullName?.familyName
+                            
+                            self.email = email ?? ""
+                            self.firstName = firstName ?? ""
+                            self.lastName = lastName ?? ""
+                            self.userID = userId
+                        default:
+                            print("errror")
+                            
+                        }
+                   
+                        
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+                .signInWithAppleButtonStyle(
+                    colorScheme == .dark ? .white : .black
+                )
+                .frame (height: 50 )
+                .padding()
+                .cornerRadius(8)
+                
+                
+            }.navigationTitle("SignIn")
+        }
     }
 }
 
